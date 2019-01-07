ac231a08a7823fbfd450c2a683bfec2fbeb42145
Mike Harvey <mharvey25@gmail.com>
2019-01-07 16:59:43 -0700

    pushing to heroku

--- models/burger.js
+++ models/burger.js
@@ -6,19 +6,19 @@ var orm = require("../config/orm.js");
 
 
 var burger = {
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
+  updateOne: function (objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
       cb(res);
     });
   }
